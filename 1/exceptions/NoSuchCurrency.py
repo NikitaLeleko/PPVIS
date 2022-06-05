16ec626cb686cde93f918fb991ef138ed1f3f19e
Nikita Leleko <73548163+NikitaLeleko@users.noreply.github.com>
2022-06-05 12:46:41 +0300

    Delete 1 directory

--- 1/exceptions/NoSuchCurrency.py
+++
@@ -1,9 +0,0 @@
-class NoSuchCurrencyException(Exception):
-    def __init__(self, *args):
-        if args:
-            self.massage = args[0]
-        else:
-            self.massage = "NO SUCH CURRENCY"
-
-    def __str__(self):
-        return self.massage
--- 1/exceptions/NotEnoughMoney.py
+++
@@ -1,9 +0,0 @@
-class NotEnoughMoneyException(Exception):
-    def __init__(self, *args):
-        if args:
-            self.massage = args[0]
-        else:
-            self.massage = "NOT ENOUGH MONEY"
-
-    def __str__(self):
-        return self.massage
