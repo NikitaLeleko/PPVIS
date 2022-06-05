2bed6795ecfd495a5bf8fe38752a2137b49756c2
Nikita Leleko <73548163+NikitaLeleko@users.noreply.github.com>
2022-06-05 12:50:02 +0300

    Delete 1 directory

--- 1/exceptions/CardIsBlocked.py
+++
@@ -1,9 +0,0 @@
-class CardIsBlockedException(Exception):
-    def __init__(self, *args):
-        if args:
-            self.massage = args[0]
-        else:
-            self.massage = "CARD IS DEACTIVATED"
-
-    def __str__(self):
-        return self.massage
